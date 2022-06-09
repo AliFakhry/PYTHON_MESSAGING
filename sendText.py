1912686d5886bbfe937d08c15f3fe7c0f929b829
Ali Fakhry <54952763+AliFakhry@users.noreply.github.com>
2022-06-08 23:27:14 -0700

    Delete sendText.py

--- sendText.py
+++
@@ -1,9 +0,0 @@
-from main import return_carrier
-from main import send_sms_via_email
-
-def send_msg(number):
-
-    message = "Hi"
-    provider = return_carrier(number)
-    sender_credentials = ("fakhrysms@gmail.com", "edojjjulohwtcgmd")
-    send_sms_via_email(number, message, provider, sender_credentials)
